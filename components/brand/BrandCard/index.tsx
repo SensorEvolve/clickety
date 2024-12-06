3d171655c61f0fdaab5f5879aba2648ac27d3854
Carlos Juan <152402940+SensorEvolve@users.noreply.github.com>
2024-12-06 14:07:43 +0100

    feat: add BrandCard component

--- components/brand/BrandCard/index.tsx
+++ components/brand/BrandCard/index.tsx
@@ -1,35 +1,9 @@
-import { StyleSheet, Pressable } from "react-native";
-import { View, Text } from "@/components/Themed";
-import { router } from "expo-router";
+import { View, Text } from "react-native";
 
-export default function BrandCard({ name }: { name: string }) {
+export function BrandCard({ name }: { name: string }) {
   return (
-    <Pressable
-      style={styles.card}
-      onPress={() => router.push(`/brand/${name}`)}
-    >
-      <View style={styles.inner}>
-        <Text style={styles.title}>{name}</Text>
-      </View>
-    </Pressable>
+    <View>
+      <Text>{name}</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  card: {
-    width: "48%",
-    margin: "1%",
-    aspectRatio: 1,
-  },
-  inner: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 8,
-    backgroundColor: "#f0f0f0",
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
