8e82cc3cd8a4e6f02d0200c928b38ac7163606bf
Carlos Juan <152402940+SensorEvolve@users.noreply.github.com>
2024-12-06 14:00:18 +0100

    fix: add proper data handling in detail screen

--- app/(tabs)/brand/[id].tsx
+++ app/(tabs)/brand/[id].tsx
@@ -1,12 +1,26 @@
 import { useLocalSearchParams } from 'expo-router';
 import { View } from 'react-native';
 import { SwitchDetail } from '../../../components/SwitchDetail';
+import { Switch } from '../../../types';
 
 export default function DetailScreen() {
   const { id } = useLocalSearchParams();
+  
+  // TODO: Get actual data from CSV
+  const data: Switch = {
+    name: "Cherry",
+    product: "MX Red",
+    type: "Linear",
+    actuationForce: 45,
+    totalTravel: 4.0,
+    actuationPoint: 2.0,
+    durability: "100 million",
+    price: 10
+  };
+
   return (
     <View>
-      <SwitchDetail data={switchData} />
+      <SwitchDetail data={data} />
     </View>
   );
 }
